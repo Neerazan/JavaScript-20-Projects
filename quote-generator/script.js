1d3c40100785121c8e725c9b8dfc147d6eb4cb41
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2023-08-10 21:34:28 +0545

    tweet and next quote button working

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,12 +1,28 @@
 const quoteContainer = document.getElementById('quote-container');
-
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 let apiQuotes = []
 
 //show new quote
 function newQuote(){
     //pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    //check if author filed is blank and replace it with 'unknown'
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }
+    else{
+        authorText.textContent = quote.author;
+    }
+    //check quote lenght to determine styling
+    if(quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -21,6 +37,17 @@ async function getQuotes(){
         alert("Something error...")
     }
 }
+
+//tweet quote
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+// event listeaners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote)
 
 //On Load
 getQuotes()
