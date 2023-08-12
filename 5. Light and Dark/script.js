b91b0f4a1ac2a46880170cf2750e7abd10736c61
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2023-08-12 18:09:21 +0545

    complete dark light mode

--- 5. Light and Dark/script.js
+++ 5. Light and Dark/script.js
@@ -1,4 +1,35 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 =  document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+//Dark and Light Images
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+//Dark Mode Styles
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun', 'fa-moon');
+    imageMode('dark');
+}
+
+//Light Mode Styles
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
 
 
 //Switch theme Dynamically
@@ -6,9 +37,11 @@ function switchTheme(event) {
     if(event.target.checked){
         // document.documentElement.setAttribute('text-size', 'bigg');
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
     }else{
         // document.documentElement.setAttribute('text-size', 'small');
         document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
     }
 }
 
