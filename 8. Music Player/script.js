95e202ba4e82b2bd94705a5189d14cec37b27916
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2023-08-14 14:49:04 +0545

    next and previous button

--- 8. Music Player/script.js
+++ 8. Music Player/script.js
@@ -2,6 +2,28 @@ const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+
+//Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Kill Em With Kindness',
+        artist: 'Selena Gomez'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Dusk Till Dawn',
+        artist: 'Zayn Malik'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design'
+    }
+]
 
 //Check if playing
 let isPlaying = false;
@@ -23,3 +45,42 @@ function pauseSong() {
 
 //Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+let songIndex = 0;
+
+//Previous Song
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0) {
+        songIndex = songs.length -1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+//Next Song
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length - 1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+
+//On Load - Select First Song
+loadSong(songs[songIndex]);
+
+//Event Listeaners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
