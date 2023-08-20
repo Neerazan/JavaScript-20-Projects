8f26598f5f53226d00ed9ad497be4e7b24318ea7
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2023-08-20 20:03:40 +0545

    populating cureent time and duration

--- 11. Video Player/script.js
+++ 11. Video Player/script.js
@@ -6,7 +6,7 @@ const volumeIcon = document.getElementById('volume-icon');
 const volumeRange = document.querySelector('.volume-range');
 const volumeBar =  document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
-const duration = document.querySelector('time-duration');
+const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullScreen');
 
 // Play & Pause ----------------------------------- //
@@ -31,9 +31,20 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+
+// calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time/60);
+    let seconds =  Math.floor(time%60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
+
 // Update progress bar as video plays
 function updateProgress() {
     progressBar.style.width = `${(video.currentTime/video.duration)*100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)}/`
+    duration.textContent = `${displayTime(video.duration)}`
 }
 
 // Volume Controls --------------------------- //
@@ -52,3 +63,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+// progressRange.addEventListener('click', setProgress);
