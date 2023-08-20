51f2370a74029ac8ff3be088eb76a1f22130c36c
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2023-08-20 20:50:52 +0545

    playback speed

--- 11. Video Player/script.js
+++ 11. Video Player/script.js
@@ -8,6 +8,7 @@ const volumeBar =  document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullScreen');
+const speed = document.querySelector('.player-speed');
 
 // Play & Pause ----------------------------------- //
 
@@ -104,6 +105,9 @@ function toggleMute() {
 
 // Change Playback Speed -------------------- //
 
+function changeSpeed() {
+    video.playbackRate = speed.value;
+}
 
 
 // Fullscreen ------------------------------- //
@@ -117,3 +121,4 @@ video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
